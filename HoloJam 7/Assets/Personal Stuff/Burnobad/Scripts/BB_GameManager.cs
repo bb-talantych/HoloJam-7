715d814dbd77c3381d0c536fd7cdf392fcd51475
Vladyslav Michurin <goduster.mail@gmail.com>
2025-12-30 08:34:39 -0500

    Game Manager
    Testing Game Manager implementation

--- HoloJam 7/Assets/Personal Stuff/Burnobad/Scripts/BB_GameManager.cs
+++ HoloJam 7/Assets/Personal Stuff/Burnobad/Scripts/BB_GameManager.cs
@@ -1,18 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using System;
 
 public class BB_GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static BB_GameManager instance;
+    public static BB_GameManager Instance
     {
-        
+        get
+        {
+            if (instance != null)
+                return instance;
+            else
+            {
+                Debug.LogError("BB_GameManager instance not setup");
+                return null;
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #region GameManager Events
+
+    public static event Action Event_GameStart;
+    public static event Action Event_GameOver;
+
+    #endregion
+
+    // All local Events must be here
+    #region On Enable/Disable
+    private void OnEnable()
     {
-        
+        Debug.Log(this.name.ToString() + " triggered OnEnable");
+
+        Event_GameStart += OnGameStart;
+        Event_GameOver += OnGameOver;
+
+        Event_GameStart?.Invoke();
     }
+
+    private void OnDisable() 
+    {
+        Debug.Log(this.name.ToString() + " triggered OnDisable");
+        Event_GameOver?.Invoke();
+
+        Event_GameStart -= OnGameStart;
+        Event_GameOver -= OnGameOver;
+    }
+
+    #endregion
+
+    // Awake only to initialize stuff once
+    private void Awake()
+    {
+        if(instance == null)
+            instance = this;
+
+
+    }
+
+    #region All Event functions
+    private void OnGameStart()
+    {
+        Debug.Log("Game Start");
+    }
+
+    private void OnGameOver()
+    {
+        Debug.Log("Game Over");
+    }
+
+    #endregion
 }
