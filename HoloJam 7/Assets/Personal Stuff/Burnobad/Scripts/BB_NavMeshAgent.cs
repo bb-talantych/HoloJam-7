ad5cabe0ccff29caed4968f70be2500a11e6ce25
Vladyslav Michurin <goduster.mail@gmail.com>
2025-12-31 13:42:48 -0500

    clicking and moving
    Somewhat working implementation

---
+++ HoloJam 7/Assets/Personal Stuff/Burnobad/Scripts/BB_NavMeshAgent.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BB_NavMeshAgent : MonoBehaviour
+{
+    public Camera cam;   
+
+    public NavMeshAgent agent;
+
+    public bool selected = false;
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider.gameObject.layer == 9)
+                {
+                    selected = true;
+                }
+                else
+                {
+                    selected = false;
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(1) && selected)
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+
+}
